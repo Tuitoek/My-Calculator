5d8e2e5be8c36aeb0edff3d7ee74e4dfb62c1cfc
Tuitoek <sjtoek@gmail.com>
2026-01-28 15:18:11 +0300

    Added styling to buttons, display & aligned items center

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonValues = ["AC", "+/-", "%", "/", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
 
-const rightSymbols = ["/","x","-","="];
+const rightSymbols = ["/","x","-","=","+"];
 const topSymbols = ["AC","+/-","%"];
 
 for(let i=0; i < buttonValues.length; i++){
@@ -9,6 +9,18 @@ for(let i=0; i < buttonValues.length; i++){
     let button = document.createElement("button");
     button.innerText = value;
 
+    //styling button colors
+    if(value == "0"){
+        button.style.width = "180px";
+        button.style.gridColumn = "span 2"
+    }
+    if(rightSymbols.includes(value)){
+        button.style.backgroundColor = "#FF9500";
+   
+    } else if(topSymbols.includes(value)){
+        button.style.backgroundColor = "#D4D4D2";
+        button.style.color = "#1C1C1C";
+    }
     // Adding button to buttons div
     document.getElementById("buttons").appendChild(button);
 }
